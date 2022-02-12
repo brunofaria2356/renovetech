78c3d13c3df8859c23d5ba1a352769ed0c975a71
Bruno Faria <contatobfimportados@gmail.com>
2022-02-12 01:27:33 -0300

    Creating component Hero + style, adding hero in page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Hero from './components/Hero';
+
 function Home() {
   return (
-    <h2>Home</h2>
+    <Hero />
   )
 }
 
---
+++ src/pages/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../assets/style/Hero.css';
+
+function Hero() {
+  return (
+    <section className="containerHero">
+      <h2 className="titleHeroOne">A RENOVE TECH surgiu com o objetivo de
+        <span className="titleColor">
+          renovar o mercado de software trazendo
+          melhor desempenho e produtividade.
+        </span>
+      </h2>
+    </section>
+  )
+}
+
+export default Hero
