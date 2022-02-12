2a3be9f330abfb180e220b68f5f4a885aa18e65e
Bruno Faria <contatobfimportados@gmail.com>
2022-02-12 01:38:51 -0300

    creating ComponentHomeTwo most style and adding in page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Hero from './components/Hero';
 import ComponentHomeOne from './components/ComponentHomeOne';
+import ComponentHomeTwo from './components/ComponentHomeTwo';
+
 
 function Home() {
   return (
     <>
       <Hero />
       <ComponentHomeOne />
+      <ComponentHomeTwo />
     </>
   )
 }
---
+++ src/pages/components/ComponentHomeTwo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../../assets/style/ComponentHomeTwo.css';
+
+function ComponentHomeTwo() {
+  return (
+    <section className="containerHomeTwo">
+      <h2 className="componentHomeTowTitle">Por que ter a RENOVE tech como parceira?</h2>
+      <div className="containerHomeArticle">
+        <article className="containerHomeTwoCard">
+          <h3 className="HomeTwoCardTitle">Agilidade no Desenvolvimento</h3>
+          <p className="HomeTwoCardParagraph">Trabalhamos com metodologias ágeis e equipes multidisciplinares que contam com processos e ferramentas embarcadas e as skills necessárias.</p>
+        </article>
+
+        <article className="containerHomeTwoCard">
+          <h3 className="HomeTwoCardTitle">Experiência e Conhecimento</h3>
+          <p className="HomeTwoCardParagraph">Ao longo da nossa história, trabalhamos com diversos segmentos e desenvolvemos o know-how necessário para atuarnos mais diversificados projetos.</p>
+        </article>
+
+        <article className="containerHomeTwoCard">
+          <h3 className="HomeTwoCardTitle">Qualidade e Alta Performance</h3>
+          <p className="HomeTwoCardParagraph">Equipe qualificada com 100% de dedicação ao negócio, envolvendo os profissionais ideais com domínio nas melhores tecnologias disponíveis.</p>
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default ComponentHomeTwo
